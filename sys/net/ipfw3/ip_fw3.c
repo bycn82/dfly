31fa5207201d6aa9179a160913184b68a957e66a
bycn82 <bycn82@yahoo.com>
2018-04-16 01:19:08 +0000

    move to ip_fw3_state

--- sys/net/ipfw3/ip_fw3.c
+++ sys/net/ipfw3/ip_fw3.c
@@ -43,19 +43,20 @@
 #endif /* INET */
 
 #include <sys/param.h>
-#include <sys/systm.h>
+#include <sys/kernel.h>
 #include <sys/malloc.h>
 #include <sys/mbuf.h>
-#include <sys/kernel.h>
-#include <sys/proc.h>
-#include <sys/socket.h>
 #include <sys/socketvar.h>
 #include <sys/sysctl.h>
+#include <sys/systimer.h>
+#include <sys/thread2.h>
+#include <sys/in_cksum.h>
+#include <sys/systm.h>
+#include <sys/proc.h>
+#include <sys/socket.h>
 #include <sys/syslog.h>
 #include <sys/ucred.h>
-#include <sys/in_cksum.h>
 #include <sys/lock.h>
-#include <sys/thread2.h>
 #include <sys/mplock2.h>
 
 #include <netinet/in.h>
@@ -85,6 +86,7 @@
 #include <net/ipfw3/ip_fw3_table.h>
 #include <net/ipfw3/ip_fw3_sync.h>
 #include <net/ipfw3_basic/ip_fw3_basic.h>
+#include <net/ipfw3_basic/ip_fw3_state.h>
 #include <net/ipfw3_nat/ip_fw3_nat.h>
 #include <net/dummynet3/ip_dummynet3.h>
 
@@ -1329,118 +1331,6 @@ ip_fw3_ctl_zero_entry(int rulenum, int log_only)
 	return (0);
 }
 
-static int
-ip_fw3_ctl_add_state(struct sockopt *sopt)
-{
-	struct ipfw_ioc_state *ioc_state;
-	ioc_state = sopt->sopt_val;
-	if (ipfw_basic_append_state_prt != NULL) {
-		struct netmsg_del dmsg;
-		bzero(&dmsg, sizeof(dmsg));
-		netmsg_init(&dmsg.base, NULL, &curthread->td_msgport,
-			0, ip_fw3_append_state_dispatch);
-		(&dmsg)->ioc_state = ioc_state;
-		netisr_domsg(&dmsg.base, 0);
-	}
-	return 0;
-}
-
-static int
-ip_fw3_ctl_delete_state(struct sockopt *sopt)
-{
-	int rulenum = 0, error;
-	if (sopt->sopt_valsize != 0) {
-		error = soopt_to_kbuf(sopt, &rulenum, sizeof(int), sizeof(int));
-		if (error) {
-			return -1;
-		}
-	}
-	struct ipfw3_context *ctx = fw3_ctx[mycpuid];
-	struct ip_fw *rule = ctx->ipfw_rule_chain;
-
-	while (rule!=NULL) {
-		if (rule->rulenum == rulenum) {
-			break;
-		}
-		rule = rule->next;
-	}
-	if (rule == NULL) {
-		return -1;
-	}
-
-	struct netmsg_del dmsg;
-	struct netmsg_base *nmsg;
-	/*
-	 * delete the state which stub is the rule
-	 * which belongs to the CPU and the rulenum
-	 */
-	bzero(&dmsg, sizeof(dmsg));
-	nmsg = &dmsg.base;
-	netmsg_init(nmsg, NULL, &curthread->td_msgport,
-			0, ip_fw3_delete_state_dispatch);
-	dmsg.rulenum = rulenum;
-	netisr_domsg(nmsg, 0);
-	return 0;
-}
-
-static int
-ip_fw3_ctl_flush_state(struct sockopt *sopt)
-{
-	struct netmsg_del dmsg;
-	struct netmsg_base *nmsg;
-	/*
-	 * delete the state which stub is the rule
-	 * which belongs to the CPU and the rulenum
-	 */
-	bzero(&dmsg, sizeof(dmsg));
-	nmsg = &dmsg.base;
-	netmsg_init(nmsg, NULL, &curthread->td_msgport,
-			0, ip_fw3_delete_state_dispatch);
-	dmsg.rulenum = 0;
-	netisr_domsg(nmsg, 0);
-	return 0;
-}
-
-static int
-ip_fw3_ctl_get_state(struct sockopt *sopt)
-{
-	struct ipfw3_state_context *state_ctx;
-	struct ipfw3_state *s;
-
-	size_t sopt_size, total_len = 0;
-	struct ipfw_ioc_state *ioc;
-
-	sopt_size = sopt->sopt_valsize;
-	ioc = (struct ipfw_ioc_state *)sopt->sopt_val;
-	/* icmp states only in CPU 0 */
-	int cpu = 0, n;
-
-	/* udp states */
-	for (cpu = 0; cpu < ncpus; cpu++) {
-		state_ctx = fw3_state_ctx[cpu];
-		for (n = 0; n < NAT_ID_MAX; n++) {
-			RB_FOREACH(s, fw3_state_tree, &state_ctx->rb_udp_out) {
-					total_len += LEN_IOC_FW3_STATE;
-					if (total_len > sopt_size)
-						goto nospace;
-					ioc->src_addr.s_addr = ntohl(s->src_addr);
-					ioc->dst_addr.s_addr = s->dst_addr;
-					ioc->src_port = s->src_port;
-					ioc->dst_port = s->dst_port;
-					ioc->cpu_id = cpu;
-					ioc->proto = IPPROTO_UDP;
-					ioc->life = s->timestamp +
-						sysctl_var_udp_timeout - time_uptime;
-					ioc++;
-			}
-		}
-	}
-
-	sopt->sopt_valsize = total_len;
-	return 0;
-nospace:
-	return 0;
-}
 /*
  * Get the ioc_rule from the sopt
  * call ip_fw3_add_rule to add the rule
@@ -1695,16 +1585,10 @@ ipfw_ctl(struct sockopt *sopt)
 			error = ip_dn_sockopt(sopt);
 			break;
 		case IP_FW_STATE_ADD:
-			error = ip_fw3_ctl_add_state(sopt);
-			break;
 		case IP_FW_STATE_DEL:
-			error = ip_fw3_ctl_delete_state(sopt);
-			break;
 		case IP_FW_STATE_FLUSH:
-			error = ip_fw3_ctl_flush_state(sopt);
-			break;
 		case IP_FW_STATE_GET:
-			error = ip_fw3_ctl_get_state(sopt);
+			error = ip_fw3_ctl_state_sockopt(sopt);
 			break;
 		case IP_FW_TABLE_CREATE:
 		case IP_FW_TABLE_DELETE:
