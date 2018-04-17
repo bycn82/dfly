f98b03fbb0d4c7134e7cb9cddf28b25cfcc29094
bycn82 <bycn82@yahoo.com>
2018-04-17 09:38:39 +0000

    1

--- sys/net/ipfw3_basic/ip_fw3_log.h
+++ sys/net/ipfw3_basic/ip_fw3_log.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2002-2009 Luigi Rizzo, Universita` di Pisa
  *
- * Copyright (c) 2015 The DragonFly Project.  All rights reserved.
+ * Copyright (c) 2015 - 2018 The DragonFly Project.  All rights reserved.
  *
  * This code is derived from software contributed to The DragonFly Project
  * by Bill Yuan <bycn82@dragonflybsd.org>
